9c71f5710a5648ac4acad1013a8d5adea9a1115a
Kathi <kathi.resch28@gmail.com>
2020-05-05 16:19:19 +0200

    Pull Request Commit

---
+++ src/app/directives/search-box-template.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SearchBoxTemplateDirective } from './search-box-template.directive';
+
+describe('SearchBoxTemplateDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SearchBoxTemplateDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/search-box-template.directive.ts
@@ -0,0 +1,8 @@
+import { Directive, TemplateRef } from '@angular/core';
+
+@Directive({
+  selector: '[appSearchBoxTemplate]'
+})
+export class SearchBoxTemplateDirective {
+  constructor(public templateRef: TemplateRef<any>) {}
+}
